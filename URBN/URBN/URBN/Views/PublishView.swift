fc0ca910d8ddf97dfff68931c1b86bd738fc6151
Ximena Cruz <ximecmerino@gmail.com>
2024-10-29 20:45:42 -0600

    Add files via upload

---
+++ URBN/URBN/URBN/Views/PublishView.swift
@@ -0,0 +1,14 @@
+//
+//  PublishView.swift
+//  URBN
+//
+//  Created by Ximena Cruz on 29/10/24.
+//
+
+import SwiftUI
+
+struct PublishView: View {
+    var body: some View {
+        Text("Publish View")
+    }
+}
